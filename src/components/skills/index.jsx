91103414c7c2c8a7b05ed03ee38d8b4d78ee1266
Imtiyaj <129411789+Imtiyaj25@users.noreply.github.com>
2023-10-04 22:55:35 +0530

    skills

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -1,28 +1,47 @@
 import React from "react";
 import { AiOutlineCloudDownload } from "react-icons/ai";
-import Section from "../shared/section";
+
+import CallToAction from "../shared/CallToAction";
 import TechIcons from "../../images/tech-icons.png";
-import CallToAction from "../../components/shared/CallToAction";
+import Section from "../shared/section";
+import Resume from "../../download/file-sample_150kB.pdf";
+
 import "./style.scss";
 
 const Skills = () => {
-  return (
-    <Section background="dark" id="skills">
-      <div className="skills-content-wrapper">
-        <div className="left-col">
-          <img src={TechIcons} alt="" />
-        </div>
-        <div className="right-col">
-          <h2>Skills</h2>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non
-            tempore, ad aliquam laudantium animi, magnam tempora cumque!
-          </p>
-          <CallToAction text="Download CV" icon={<AiOutlineCloudDownload/>}/>
-        </div>
-      </div>
-    </Section>
-  );
+    return (
+        <Section
+            background="dark"
+            id="skills"
+        >
+            <div className="skills-content-wrapper">
+                <div className="left-col">
+                    <img
+                        src={TechIcons}
+                        alt="JavaScript, React, CSS, HTML, SASS, Photoshop, Figma"
+                    />
+                </div>
+                <div className="right-col">
+                    <h2>Skills</h2>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and
+                        scrambled it to make a type specimen book. It has
+                        survived not only five centuries, but also the leap into
+                        electronic typesetting, remaining essentially unchanged.
+                        It was popularised in the 1960s with the
+                    </p>
+                    <CallToAction
+                        text="Download CV"
+                        icon={<AiOutlineCloudDownload />}
+                        action={() => window.open(Resume)}
+                    />
+                </div>
+            </div>
+        </Section>
+    );
 };
 
 export default Skills;
