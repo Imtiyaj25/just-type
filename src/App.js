d5d7fe70b69e93ce9e430510cfe28312073ed3f3
Imtiyaj <129411789+Imtiyaj25@users.noreply.github.com>
2023-10-04 21:56:59 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Intro from "../src/components/intro"
 import Skills from './components/skills';
+import Portfolio from "./components/portfolio";
+
 import "./app.scss"
+
 const App = () => {
   return (
     <div>
       <Intro/>
       <Skills/>
+      <Portfolio/>
     </div>
   )
 }
---
+++ src/components/portfolio/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Section from "../shared/section";
+import "./style.scss";
+
+const Portfolio = () => {
+  return (
+    <Section id="portfolio" title="Check my portfolio" background="light">
+        <div className="portfolio-content-wrapper"></div>
+    </Section>
+  )
+}
+
+export default Portfolio
