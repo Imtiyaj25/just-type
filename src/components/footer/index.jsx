7ffa69f55a3caf6e3638b057edc93eac87ec0b64
Imtiyaj <129411789+Imtiyaj25@users.noreply.github.com>
2023-10-10 22:50:28 +0530

    Update index.jsx

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -52,7 +52,30 @@ const Footer = () => {
                         Contact me
                     </li>
                 </ul>
-                
+                <div className="social-icons">
+                    <SocialIcon
+                        color="#FF0000"
+                        icon={<FaYoutube />}
+                        link="https://www.youtube.com/"
+                    />
+                    <SocialIcon
+                        color="#0D2636"
+                        icon={<FaGithub />}
+                        link="https://github.com/Imtiyaj25"
+                    />
+                    <SocialIcon
+                        color="#0A66C2"
+                        icon={<FaLinkedinIn />}
+                    />
+                    <SocialIcon
+                        color="#f2740d"
+                        icon={<FaStackOverflow />}
+                    />
+                    <SocialIcon
+                        color="#E84C88"
+                        icon={<FaInstagram />}
+                    />
+                </div>
                 <div className="bottom-bar">
                     <div className="copyright-text">
                         Copyright 2023 Imtiyaj Sayyad | All Rights Reserved
