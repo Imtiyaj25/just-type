2701001a9869bc17cbbbc27173e19073070ff992
Imtiyaj <129411789+Imtiyaj25@users.noreply.github.com>
2023-10-10 22:50:20 +0530

    Update index.jsx

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import {
+    FaYoutube,
+    FaGithub,
+    FaLinkedinIn,
+    FaStackOverflow,
+    FaInstagram,
+} from "react-icons/fa";
 
-const FooterSection = () => {
-  return (
-    <div>FooterSection</div>
-  )
-}
+import Section from "../shared/section";
+import Logo from "../../images/logo.png";
+import SocialIcon from "./social-icon";
+import { scrollToSection } from "../utils/helpers";
 
-export default FooterSection
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <Section
+            background="dark"
+            className="footer"
+        >
+            <div className="footer-content-wrapper">
+                <div className="footer-logo">
+                <a>Say<span className="logo">yad.</span></a>
+                </div>
+                <ul className="footer-menu-items">
+                    <li
+                        className="footer-menu-item"
+                        onClick={() => scrollToSection("skills")}
+                    >
+                        Skills
+                    </li>
+                    <li
+                        className="footer-menu-item"
+                        onClick={() => scrollToSection("portfolio")}
+                    >
+                        Portfolio
+                    </li>
+                    <li
+                        className="footer-menu-item"
+                        onClick={() => scrollToSection("blogs")}
+                    >
+                        Blogs & Articles
+                    </li>
+                    <li
+                        className="footer-menu-item"
+                        action={() => {
+                            scrollToSection("contact");
+                        }}
+                    >
+                        Contact me
+                    </li>
+                </ul>
+                
+                <div className="bottom-bar">
+                    <div className="copyright-text">
+                        Copyright 2023 Imtiyaj Sayyad | All Rights Reserved
+                    </div>
+                </div>
+            </div>
+        </Section>
+    );
+};
+
+export default Footer;
