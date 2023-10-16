705c42c5219596201e15aa8b7509cce92b581dba
Imtiyaj Sayyad <imsayyad97@gmail.com>
2023-10-16 15:32:19 +0530

    Update index.jsx

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -28,7 +28,7 @@ const Skills = () => {
                         typesetting industry. Lorem Ipsum has been the
                         industry's standard dummy text ever since the 1500s,
                         when an unknown printer took a galley of type and
-                        scrambled it to make a type specimen book.
+                        it to make a type specimen book.
                     </p>
                     <CallToAction
                         text="Download CV"
